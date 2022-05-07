add6d46a3ca13e05407baf13d74c5a18bed6fd47
androjm <androjm@gmail.com>
2022-05-07 17:31:13 +0200

    Comienzo del curso

---
+++ moisset-python/for8.py
@@ -0,0 +1,7 @@
+'''
+Programa que muestre la tabla de multiplicar del 5(del 5 al 50)
+'''
+print("Tabla de multiplicar del 5")
+for tabla in range(5, 51, 5):#empieza en 5, termina en 50, y salta de 5 en 5
+    print(tabla)
+    
